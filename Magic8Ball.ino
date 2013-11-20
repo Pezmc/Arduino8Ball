6ac709fc0b5faf18ad270f2bd9133cc34f03f6dd
Pez Cuckow <email@pezcuckow.com>
2013-11-20 15:35:48 +0000

    Our Final Code

--- Magic8Ball.ino
+++ Magic8Ball.ino
@@ -1,13 +1,19 @@
+#include <LiquidCrystal.h>
+#include <Adafruit_NeoPixel.h>
+#include "pitches.h"
 
+#define PIN 9
 // constants won't change. They're used here to 
 // set pin numbers:
 const int buttonPin = 8;    // the number of the pushbutton pin
-const int ledPin = 13;      // the number of the LED pin
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 // Variables will change:
-int ledState = HIGH;         // the current state of the output pin
 int buttonState;             // the current reading from the input pin
 int lastButtonState = LOW;   // the previous reading from the input pin
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(16, PIN, NEO_GRB + NEO_KHZ800);
 
 // the following variables are long's because the time, measured in miliseconds,
 // will quickly become a bigger number than can be stored in an int.
@@ -18,7 +24,14 @@ void setup() {
   pinMode(buttonPin, INPUT);
   //pinMode(ledPin, OUTPUT);
   
+    // set up the LCD's number of columns and rows: 
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("hello, world!");
+
   Serial.begin(9600);
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
 
   // set initial LED state
   //digitalWrite(ledPin, ledState);
@@ -61,6 +74,17 @@ void loop() {
   // save the reading.  Next time through the loop,
   // it'll be the lastButtonState:
   lastButtonState = reading;
+  
+ 
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, c);
+      strip.show();
+      delay(wait);
+  }
 }
 
 int answerCount = 20;
@@ -87,11 +111,69 @@ char* answers[] = {
   "Very doubtful"
 };
 
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  if(WheelPos < 85) {
+   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if(WheelPos < 170) {
+   WheelPos -= 85;
+   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else {
+   WheelPos -= 170;
+   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
 // Write a random message to the LED display
 void displayRandomMessage(){
-  Serial.print("8Ball:");
-  Serial.print(answers[random(0,answerCount-1)]);
-  Serial.println(" .");
+  
+  tone(10, NOTE_A4, 3000);
+  
+  strip.setBrightness(255);
+   colorWipe(strip.Color(255, 0, 0), 50); // Red
+  colorWipe(strip.Color(0, 255, 0), 50); // Green
+  colorWipe(strip.Color(0, 0, 255), 50); // Blue
+  rainbow(10);
+  
+  int i;
+  strip.setBrightness(100);
+  for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + 5) & 255));
+  }
+  strip.show();
+  //delay(5000);
+  
+  strip.setBrightness(0);
+  strip.show();
+  
+  lcd.clear();
+  char * answer= answers[random(0,answerCount-1)];
+  char buffer[17];
+  strncpy(buffer, answer, 16);
+  lcd.print(buffer);
+  if (strlen (answer) > 16)
+  {
+    lcd.setCursor(0, 2);
+    strncpy(buffer, answer+16, 16);  
+    lcd.print(buffer);
+  }
   delay(1);        // delay in between reads for stability
 }
+
+
+
+
+
 
